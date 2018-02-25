eb14a047a2d0bf88675a517662d2e596078d90d8
Wismut <codename__47@ukr.net>
2018-02-25 18:24:06 +0200

    add JUnit 4

---
+++ test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,34 @@
+package ru.javawebinar.webapp.storage;
+
+import static org.junit.Assert.*;
+
+public class ArrayStorageTest {
+
+	@org.junit.Test
+	public void clear() {
+	}
+
+	@org.junit.Test
+	public void save() {
+	}
+
+	@org.junit.Test
+	public void update() {
+	}
+
+	@org.junit.Test
+	public void load() {
+	}
+
+	@org.junit.Test
+	public void delete() {
+	}
+
+	@org.junit.Test
+	public void getAllSorted() {
+	}
+
+	@org.junit.Test
+	public void size() {
+	}
+}
