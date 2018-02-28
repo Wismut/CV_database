10a58aaf1b8643e83e27493190293725c031e841
Wismut <codename__47@ukr.net>
2018-02-28 14:06:43 +0200

    add WebAppException

--- src/ru/javawebinar/webapp/storage/ArrayStorage.java
+++ src/ru/javawebinar/webapp/storage/ArrayStorage.java
@@ -6,31 +6,58 @@ import java.util.Collection;
 
 public class ArrayStorage implements IStorage {
 
-	private Resume[] array = new Resume[100];
+	private static final int LIMIT = 100;
+	private Resume[] array = new Resume[LIMIT];
 
 	@Override
 	public void clear() {
-
+		for (int i = 0; i < LIMIT; i++) {
+			array[i] = null;
+		}
 	}
 
 	@Override
 	public void save(Resume r) {
-
+		for (int i = 0; i < LIMIT; i++) {
+			Resume resume = array[i];
+			if (resume != null) {
+				if (resume.equals(r)) throw new RuntimeException("Already present");
+			} else {
+				array[i] = r;
+				return;
+			}
+		}
 	}
 
 	@Override
 	public void update(Resume r) {
-
+		for (int i = 0; i < LIMIT; i++) {
+			if (array[i] != null && array[i].equals(r)) {
+				array[i] = r;
+				return;
+			}
+		}
 	}
 
 	@Override
 	public Resume load(String uuid) {
+		if (uuid == null) return null;
+		for (int i = 0; i < LIMIT; i++) {
+			if (array[i] != null && uuid.equals(array[i].getUuid()))
+				return array[i];
+		}
 		return null;
 	}
 
 	@Override
 	public void delete(String uuid) {
-
+		if (uuid == null) return;
+		for (int i = 0; i < LIMIT; i++) {
+			if (array[i] != null && uuid.equals(array[i].getUuid())) {
+				array[i] = null;
+				return;
+			}
+		}
 	}
 
 	@Override
@@ -40,6 +67,10 @@ public class ArrayStorage implements IStorage {
 
 	@Override
 	public int size() {
-		return 0;
+		int count = 0;
+		for (int i = 0; i < LIMIT; i++) {
+			if (array[i] != null) count++;
+		}
+		return count;
 	}
 }
--- test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
@@ -1,27 +1,80 @@
 package ru.javawebinar.webapp.storage;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import ru.javawebinar.webapp.model.Contact;
+import ru.javawebinar.webapp.model.ContactType;
+import ru.javawebinar.webapp.model.Resume;
 
 public class ArrayStorageTest {
 
+	private static Resume R1, R2, R3;
+
+	private ArrayStorage storage = new ArrayStorage();
+
+	static {
+		R1 = new Resume("полное имя1", "location1");
+		R1.addContact(new Contact(ContactType.MAIL, "erfg@dfg.com"));
+		R1.addContact(new Contact(ContactType.PHONE, "23453455"));
+		R2 = new Resume("полное имя2", null);
+		R2.addContact(new Contact(ContactType.MAIL, "eredrfgedfg@dfg.com"));
+		R2.addContact(new Contact(ContactType.PHONE, "2344564553455"));
+		R3 = new Resume("полное имяr564553", null);
+//		R3.addContact(new Contact(ContactType.MAIL, "erf007g@dfg.com"));
+//		R3.addContact(new Contact(ContactType.PHONE, "203455"));
+	}
+
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println(R1.getUuid());
+		System.out.println(R2.getUuid());
+		System.out.println(R3.getUuid());
+		// same as static block
+	}
+
+	@Before
+	public void before() {
+		storage.clear();
+		storage.save(R1);
+		storage.save(R2);
+		storage.save(R3);
+	}
+
 	@org.junit.Test
 	public void clear() {
+		storage.clear();
+		Assert.assertEquals(0, storage.size());
 	}
 
 	@org.junit.Test
 	public void save() {
+		storage.clear();
+		storage.save(R1);
+		Assert.assertEquals(R1, storage.load(R1.getUuid()));
 	}
 
 	@org.junit.Test
 	public void update() {
+		storage.update(new Resume(R2.getUuid(), "new full name", "loca"));
+		Resume resume = storage.load(R2.getUuid());
+		Assert.assertEquals(resume.getFullName(), "new full name");
+		Assert.assertEquals(resume.getLocation(), "loca");
 	}
 
 	@org.junit.Test
 	public void load() {
+		Resume resume = storage.load(R3.getUuid());
+		Assert.assertEquals(R3.getLocation(), resume.getLocation());
+		Assert.assertEquals(R3.getFullName(), resume.getFullName());
+		Assert.assertEquals(R3.getUuid(), resume.getUuid());
 	}
 
 	@org.junit.Test
 	public void delete() {
+		storage.delete(R1.getUuid());
+		Assert.assertEquals(2, storage.size());
+		Assert.assertEquals(null, storage.load(R1.getUuid()));
 	}
 
 	@org.junit.Test
@@ -30,5 +83,6 @@ public class ArrayStorageTest {
 
 	@org.junit.Test
 	public void size() {
+		Assert.assertEquals(3, storage.size());
 	}
 }
