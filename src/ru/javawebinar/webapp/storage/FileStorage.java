887ec16f655a923e306244390479465ad9289df0
Max <maksym.ivanov@leroymerlin.ua>
2019-01-30 14:47:33 +0200

    Implements read and write resume

--- src/ru/javawebinar/webapp/storage/FileStorage.java
+++ src/ru/javawebinar/webapp/storage/FileStorage.java
@@ -1,68 +1,112 @@
 package ru.javawebinar.webapp.storage;
 
 import ru.javawebinar.webapp.WebAppException;
+import ru.javawebinar.webapp.model.ContactType;
 import ru.javawebinar.webapp.model.Resume;
+import ru.javawebinar.webapp.model.Section;
+import ru.javawebinar.webapp.model.SectionType;
 
-import java.io.File;
+import java.io.*;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class FileStorage extends AbstractStorage<File> {
 
-	private File dir;
+    private File dir;
 
-	public FileStorage(String path) {
-		this.dir = new File(path);
-		if (!dir.isDirectory() || !dir.canWrite())
-			throw new IllegalArgumentException("'" + path + "' is not a directory or is not writable");
-	}
+    public FileStorage(String path) {
+        this.dir = new File(path);
+        if (!dir.isDirectory() || !dir.canWrite())
+            throw new IllegalArgumentException("'" + path + "' is not a directory or is not writable");
+    }
 
-	@Override
-	protected void doSave(File context, Resume r) {
+    @Override
+    protected void doSave(File file, Resume r) {
+        try {
+            file.createNewFile();
+            write(file, r);
+        } catch (IOException e) {
+            throw new WebAppException("Couldn't create file " + file.getAbsolutePath(), r, e);
+        }
+    }
 
-	}
+    protected void write(File file, Resume r) {
+        try (FileOutputStream fos = new FileOutputStream(file);
+             DataOutputStream dos = new DataOutputStream(fos)) {
+            dos.writeUTF(r.getFullName());
+            dos.writeUTF(r.getLocation());
+            dos.writeUTF(r.getHomePage());
+            for (Map.Entry<ContactType, String> contact : r.getContacts().entrySet()) {
+                dos.writeInt(contact.getKey().ordinal());
+                dos.writeUTF(contact.getValue());
+            }
+            for (Map.Entry<SectionType, Section> section : r.getSections().entrySet()) {
+                dos.writeInt(section.getKey().ordinal());
+                dos.writeUTF(section.getValue().toString());
+            }
+        } catch (IOException e) {
+            throw new WebAppException("Couldn't write file " + file.getAbsolutePath(), r, e);
+        }
+    }
 
-	@Override
-	protected File getContext(String fileName) {
-		return new File(fileName);
-	}
+    protected Resume read(File file) {
+        Resume r = new Resume();
+        try (InputStream is  = new FileInputStream(file); DataInputStream dis = new DataInputStream(is)) {
+            r.setFullName(dis.readUTF());
+            r.setLocation(dis.readUTF());
+            r.setHomePage(dis.readUTF());
+            Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
+            // TODO: 1/30/2019
+        } catch (IOException e) {
 
-	@Override
-	protected boolean exist(File file) {
-		return file.exists();
-	}
+        }
+        return r;
+    }
 
-	@Override
-	protected void doClear() {
-		File[] files = dir.listFiles();
-		if (files == null) return;
-		for (File file : files) {
-			doDelete(file);
-		}
-	}
+    @Override
+    protected File getContext(String fileName) {
+        return new File(fileName);
+    }
 
-	@Override
-	protected void doUpdate(File context, Resume r) {
+    @Override
+    protected boolean exist(File file) {
+        return file.exists();
+    }
 
-	}
+    @Override
+    protected void doClear() {
+        File[] files = dir.listFiles();
+        if (files == null) return;
+        for (File file : files) {
+            doDelete(file);
+        }
+    }
 
-	@Override
-	public Resume doLoad(File context) {
-		return null;
-	}
+    @Override
+    protected void doUpdate(File file, Resume r) {
+        write(file, r);
+    }
 
-	@Override
-	protected void doDelete(File file) {
-		if (file.delete())
-			throw new WebAppException("File " + file.getAbsolutePath() + " can not be deleted");
-	}
+    @Override
+    public Resume doLoad(File file) {
+        return read(file);
+    }
 
-	@Override
-	protected List<Resume> doGetAll() {
-		return null;
-	}
+    @Override
+    protected void doDelete(File file) {
+        if (file.delete())
+            throw new WebAppException("File " + file.getAbsolutePath() + " can not be deleted");
+    }
 
-	@Override
-	public int size() {
-		return 0;
-	}
+    @Override
+    protected List<Resume> doGetAll() {
+        File[] files = dir.listFiles();
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
