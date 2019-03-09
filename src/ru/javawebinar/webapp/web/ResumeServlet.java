8df4a658059721ba261cdda19aec9946599d7b78
Max <maksym.ivanov@leroymerlin.ua>
2019-03-09 16:24:28 +0200

    Implements doGet in ResumeServlet

--- src/ru/javawebinar/webapp/web/ResumeServlet.java
+++ src/ru/javawebinar/webapp/web/ResumeServlet.java
@@ -1,5 +1,25 @@
 package ru.javawebinar.webapp.web;
 
-public class ResumeServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
 
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+        Writer w = resp.getWriter();
+        String name = req.getParameter("name");
+        w.write("Тест сервлет: привет " + name);
+        w.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
